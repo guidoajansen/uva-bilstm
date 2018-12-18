d80a69e1498089d43a7154e0f1f39949bf516bd9
Guido Jansen <guido.a.jansen@gmail.com>
2018-12-18 16:20:18 +0100

    Initla work on transforming to TF

--- model.py
+++ model.py
@@ -12,4 +12,23 @@ import tensorflow as tf
 
 """Load external scripts"""
 from validation import compute_f1
-import models.bilstm
+import models.bilstm as bilstm
+
+"""Set parameters"""
+EPOCHS = 30               # paper: 80
+DROPOUT = 0.5             # paper: 0.68
+DROPOUT_RECURRENT = 0.25  # not specified in paper, 0.25 recommended
+LSTM_STATE_SIZE = 200     # paper: 275
+CONV_SIZE = 3             # paper: 3
+LEARNING_RATE = 0.0105    # paper 0.0105
+OPTIMIZER = 'nadam'       # paper uses SGD(lr=self.learning_rate), Nadam() recommended
+
+"""Construct and run model"""
+cnn_blstm = bilstm.CNN_BLSTM(EPOCHS, DROPOUT, DROPOUT_RECURRENT, LSTM_STATE_SIZE, CONV_SIZE, LEARNING_RATE, OPTIMIZER)
+cnn_blstm.loadData()
+cnn_blstm.addCharInfo()
+cnn_blstm.embed()
+# cnn_blstm.createBatches()
+# cnn_blstm.buildModel()
+# cnn_blstm.train()
+# cnn_blstm.writeToFile()
--- models/bilstm.py
+++ models/bilstm.py
@@ -1,6 +1,11 @@
 """Initialise class"""
 
-from preprocessing import readfile, createBatches, createMatrices, iterate_minibatches, addCharInformation, padding
+import numpy as np
+import os.path
+
+# from models.preprocessing import readfile, createBatches, createMatrices, iterate_minibatches, addCharInformation, padding
+from models.preprocessing import readfile, addCharInformation
+
 
 class CNN_BLSTM(object):
 
@@ -14,11 +19,11 @@ class CNN_BLSTM(object):
         self.learning_rate = LEARNING_RATE
         self.optimizer = OPTIMIZER
 
+    """Load data and add character information"""
     def loadData(self):
-        """Load data and add character information"""
-        self.trainSentences = readfile("data/train.txt")
-        self.devSentences = readfile("data/dev.txt")
-        self.testSentences = readfile("data/test.txt")
+        self.trainSentences = readfile("train.txt")
+        self.devSentences = readfile("dev.txt")
+        self.testSentences = readfile("test.txt")
 
     def addCharInfo(self):
         # format: [['EU', ['E', 'U'], 'B-ORG\n'], ...]
@@ -54,168 +59,38 @@ class CNN_BLSTM(object):
         word2Idx = {}
         self.wordEmbeddings = []
 
-        fEmbeddings = open("embeddings/glove.6B.50d.txt", encoding="utf-8")
+        directory = os.path.join(os.path.dirname(__file__), os.pardir + '/embeddings/glove/')
+        fEmbeddings = open(directory + 'glove.6B.300d.txt', encoding = 'utf-8')
 
         # loop through each word in embeddings
         for line in fEmbeddings:
             split = line.strip().split(" ")
             word = split[0]  # embedding word entry
 
-            if len(word2Idx) == 0:  # add padding+unknown
-                word2Idx["PADDING_TOKEN"] = len(word2Idx)
-                vector = np.zeros(len(split) - 1)  # zero vector for 'PADDING' word
-                self.wordEmbeddings.append(vector)
-
-                word2Idx["UNKNOWN_TOKEN"] = len(word2Idx)
-                vector = np.random.uniform(-0.25, 0.25, len(split) - 1)
-                self.wordEmbeddings.append(vector)
-
-            if split[0].lower() in words:
-                vector = np.array([float(num) for num in split[1:]])
-                self.wordEmbeddings.append(vector)  # word embedding vector
-                word2Idx[split[0]] = len(word2Idx)  # corresponding word dict
-
-        self.wordEmbeddings = np.array(self.wordEmbeddings)
-
-        # dictionary of all possible characters
-        self.char2Idx = {"PADDING": 0, "UNKNOWN": 1}
-        for c in " 0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ.,-_()[]{}!?:;#'\"/\\%$`&=*+@^~|<>":
-            self.char2Idx[c] = len(self.char2Idx)
-
-        # format: [[wordindices], [caseindices], [padded word indices], [label indices]]
-        self.train_set = padding(createMatrices(self.trainSentences, word2Idx, self.label2Idx, case2Idx, self.char2Idx))
-        self.dev_set = padding(createMatrices(self.devSentences, word2Idx, self.label2Idx, case2Idx, self.char2Idx))
-        self.test_set = padding(createMatrices(self.testSentences, word2Idx, self.label2Idx, case2Idx, self.char2Idx))
-
-        self.idx2Label = {v: k for k, v in self.label2Idx.items()}
-
-    def createBatches(self):
-        """Create batches"""
-        self.train_batch, self.train_batch_len = createBatches(self.train_set)
-        self.dev_batch, self.dev_batch_len = createBatches(self.dev_set)
-        self.test_batch, self.test_batch_len = createBatches(self.test_set)
-
-    def tag_dataset(self, dataset, model):
-        """Tag data with numerical values"""
-        correctLabels = []
-        predLabels = []
-        for i, data in enumerate(dataset):
-            tokens, casing, char, labels = data
-            tokens = np.asarray([tokens])
-            casing = np.asarray([casing])
-            char = np.asarray([char])
-            pred = model.predict([tokens, casing, char], verbose=False)[0]
-            pred = pred.argmax(axis=-1)  # Predict the classes
-            correctLabels.append(labels)
-            predLabels.append(pred)
-        return predLabels, correctLabels
-
-    def buildModel(self):
-        """Model layers"""
-
-        # character input
-        character_input = Input(shape=(None, 52,), name="Character_input")
-        embed_char_out = TimeDistributed(
-            Embedding(len(self.char2Idx), 30, embeddings_initializer=RandomUniform(minval=-0.5, maxval=0.5)), name="Character_embedding")(
-            character_input)
-
-        dropout = Dropout(self.dropout)(embed_char_out)
-
-        # CNN
-        conv1d_out = TimeDistributed(Conv1D(kernel_size=self.conv_size, filters=30, padding='same', activation='tanh', strides=1), name="Convolution")(dropout)
-        maxpool_out = TimeDistributed(MaxPooling1D(52), name="Maxpool")(conv1d_out)
-        char = TimeDistributed(Flatten(), name="Flatten")(maxpool_out)
-        char = Dropout(self.dropout)(char)
-
-        # word-level input
-        words_input = Input(shape=(None,), dtype='int32', name='words_input')
-        words = Embedding(input_dim=self.wordEmbeddings.shape[0], output_dim=self.wordEmbeddings.shape[1], weights=[self.wordEmbeddings],
-                          trainable=False)(words_input)
-
-        # case-info input
-        casing_input = Input(shape=(None,), dtype='int32', name='casing_input')
-        casing = Embedding(output_dim=self.caseEmbeddings.shape[1], input_dim=self.caseEmbeddings.shape[0], weights=[self.caseEmbeddings],
-                           trainable=False)(casing_input)
-
-        # concat & BLSTM
-        output = concatenate([words, casing, char])
-        output = Bidirectional(LSTM(self.lstm_state_size,
-                                    return_sequences=True,
-                                    dropout=self.dropout,                        # on input to each LSTM block
-                                    recurrent_dropout=self.dropout_recurrent     # on recurrent input signal
-                                   ), name="BLSTM")(output)
-        output = TimeDistributed(Dense(len(self.label2Idx), activation='softmax'),name="Softmax_layer")(output)
-
-        # set up model
-        self.model = Model(inputs=[words_input, casing_input, character_input], outputs=[output])
-
-        self.model.compile(loss='sparse_categorical_crossentropy', optimizer=self.optimizer)
-
-        self.init_weights = self.model.get_weights()
-
-        plot_model(self.model, to_file='model.png')
-
-        print("Model built. Saved model.png\n")
-
-    def train(self):
-        """Default training"""
-
-        self.f1_test_history = []
-        self.f1_dev_history = []
-
-        for epoch in range(self.epochs):
-            print("Epoch {}/{}".format(epoch, self.epochs))
-            for i,batch in enumerate(iterate_minibatches(self.train_batch,self.train_batch_len)):
-                labels, tokens, casing,char = batch
-                self.model.train_on_batch([tokens, casing,char], labels)
-
-            # compute F1 scores
-            predLabels, correctLabels = self.tag_dataset(self.test_batch, self.model)
-            pre_test, rec_test, f1_test = compute_f1(predLabels, correctLabels, self.idx2Label)
-            self.f1_test_history.append(f1_test)
-            print("f1 test ", round(f1_test, 4))
-
-            predLabels, correctLabels = self.tag_dataset(self.dev_batch, self.model)
-            pre_dev, rec_dev, f1_dev = compute_f1(predLabels, correctLabels, self.idx2Label)
-            self.f1_dev_history.append(f1_dev)
-            print("f1 dev ", round(f1_dev, 4), "\n")
-
-        print("Final F1 test score: ", f1_test)
-
-        print("Training finished.")
-
-        # save model
-        self.modelName = "{}_{}_{}_{}_{}_{}_{}".format(self.epochs,
-                                                        self.dropout,
-                                                        self.dropout_recurrent,
-                                                        self.lstm_state_size,
-                                                        self.conv_size,
-                                                        self.learning_rate,
-                                                        self.optimizer.__class__.__name__
-                                                       )
-
-        modelName = self.modelName + ".h5"
-        self.model.save(modelName)
-        print("Model weights saved.")
-
-        self.model.set_weights(self.init_weights)  # clear model
-        print("Model weights cleared.")
-
-    def writeToFile(self):
-        """Write output to file"""
-
-        # .txt file format
-        # [epoch  ]
-        # [f1_test]
-        # [f1_dev ]
-
-        output = np.matrix([[int(i) for i in range(self.epochs)], self.f1_test_history, self.f1_dev_history])
-
-        fileName = self.modelName + ".txt"
-        with open(fileName,'wb') as f:
-            for line in output:
-                np.savetxt(f, line, fmt='%.5f')
-
-        print("Model performance written to file.")
-
-    print("Class initialised.")
+        #     if len(word2Idx) == 0:  # add padding+unknown
+        #         word2Idx["PADDING_TOKEN"] = len(word2Idx)
+        #         vector = np.zeros(len(split) - 1)  # zero vector for 'PADDING' word
+        #         self.wordEmbeddings.append(vector)
+        #
+        #         word2Idx["UNKNOWN_TOKEN"] = len(word2Idx)
+        #         vector = np.random.uniform(-0.25, 0.25, len(split) - 1)
+        #         self.wordEmbeddings.append(vector)
+        #
+        #     if split[0].lower() in words:
+        #         vector = np.array([float(num) for num in split[1:]])
+        #         self.wordEmbeddings.append(vector)  # word embedding vector
+        #         word2Idx[split[0]] = len(word2Idx)  # corresponding word dict
+        #
+        # self.wordEmbeddings = np.array(self.wordEmbeddings)
+        #
+        # # dictionary of all possible characters
+        # self.char2Idx = {"PADDING": 0, "UNKNOWN": 1}
+        # for c in " 0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ.,-_()[]{}!?:;#'\"/\\%$`&=*+@^~|<>":
+        #     self.char2Idx[c] = len(self.char2Idx)
+        #
+        # # format: [[wordindices], [caseindices], [padded word indices], [label indices]]
+        # self.train_set = padding(createMatrices(self.trainSentences, word2Idx, self.label2Idx, case2Idx, self.char2Idx))
+        # self.dev_set = padding(createMatrices(self.devSentences, word2Idx, self.label2Idx, case2Idx, self.char2Idx))
+        # self.test_set = padding(createMatrices(self.testSentences, word2Idx, self.label2Idx, case2Idx, self.char2Idx))
+        #
+        # self.idx2Label = {v: k for k, v in self.label2Idx.items()}
